e1a0824794a36acd2bbd13545de2f83ef59a0c33
Ataberk Donmez <ataberdonmez@gmail.com>
2021-04-21 23:24:52 +0300

    minor default parameter change

--- featurizer/clustering.py
+++ featurizer/clustering.py
@@ -14,7 +14,7 @@ def read_descriptors(path):
     return df
 
 
-def read_csvs(folder="multi_task_features"):
+def read_csvs(folder):
     df = pd.DataFrame(columns=["smiles", "descriptors"])
     for filename in sorted(os.listdir(folder)):
         if filename.endswith('.csv'):
@@ -24,10 +24,30 @@ def read_csvs(folder="multi_task_features"):
     return df
 
 if __name__ == "__main__":
+    # Should I read 100 files, minibatch cluster and read another 100...?
     df = read_csvs(folder="multi_task_features_dmpnn_25_zinc_flagments")
-    descriptors = df['descriptors'].tolist()
-    smiles = df['smiles'].tolist()
     # create a dictionary
-    # why is the dictionary length different than smiles and descriptors?
-    #kmeans = MiniBatchKMeans(n_clusters=400, batch_size=400, verbose=2, ).fit(descriptors)
-    #print(kmeans.cluster_centers_)
+    smiles_descriptors_dict = dict(zip(df.smiles, df.descriptors))
+    print("Total number of molecules:", len(smiles_descriptors_dict))
+    # why is the length of this dictionary different than smiles and descriptors? Because of DUPLICATES!
+    SUBSET_SIZE = 10000000
+    start = 0
+    cs = "k-means++"
+    for i in range(SUBSET_SIZE, len(smiles_descriptors_dict)+SUBSET_SIZE, SUBSET_SIZE):
+        # print step:
+        print("ITERATION:", i/SUBSET_SIZE)
+        # set i to len(dict) if i > len(dict)
+        if i > len(smiles_descriptors_dict):
+            i = len(smiles_descriptors_dict)
+        # take a subset of the dictionary
+        print("Taking subset of the dictionary:", start, i)
+        dict_subset = {key: smiles_descriptors_dict[key] for key in list(smiles_descriptors_dict.keys())[start:i]}
+        start = i
+        # run minibatch kmeans
+        print("starting minibatch kmeans")
+        kmeans = MiniBatchKMeans(n_clusters=6000, batch_size=800, verbose=2, init=cs).fit(list(dict_subset.values()))
+        cs = kmeans.cluster_centers_
+        # create a new df with row1:smiles, row2:cluster label
+        smiles_label_df = pd.DataFrame(list(zip(dict_subset.keys(), kmeans.labels_)), columns=["smiles", "labels"])
+        # save this df as csv
+        smiles_label_df.to_csv("flagments_labels_" + str(int(i/SUBSET_SIZE)) + ".csv")
